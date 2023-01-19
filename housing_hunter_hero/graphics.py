a465b5553ee36839183014a7f5f9f82fee955db0
Brian Sward <bsward@gmail.com>
2023-01-19 15:01:22 -0800

    doc strings added

--- housing_hunter_hero/graphics.py
+++ housing_hunter_hero/graphics.py
@@ -24,6 +24,13 @@ welcome_menu = """
 
 
 def greeting():
+    """
+    Initial function which generates a menu for the user to choose which option
+    they would like to do, and then based on choice they can either search properties
+    by zip code or exit the function
+    :return: varies based on user input, but either a prompt for zip code or an exit
+    message
+    """
     print('\n'.join(l.center(80) for l in art5.splitlines()))
     print('\n'.join(l.center(80) for l in welcome_menu.splitlines()))
     choice = input("Please make a selection: \n")
